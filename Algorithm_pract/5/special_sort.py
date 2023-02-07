7b520c7fbe689fa9e4ed2a1c9e55bb2460b3f264
ah9mon <eomgyeol97@gmail.com>
2023-02-07 17:35:28 +0900

    2023-02-07

--- Algorithm_pract/5/special_sort.py
+++ Algorithm_pract/5/special_sort.py
@@ -1,6 +1,21 @@
 import sys
 sys.stdin = open('sample_input (2).txt')
 
+# T = int(input())
+
+# for i in range(1,T+1):
+#     N = int(input())
+#     num_list = list(map(int,input().split()))
+    
+#     num_list = sorted(num_list)
+#     num_list_reverse = sorted(num_list, reverse = True)
+#     new_list = []
+#     for j in range(5):
+#         new_list.append(num_list_reverse[j])
+#         new_list.append(num_list[j])
+    
+#     print(f'#{i} {" ".join(str(x) for x in new_list)}')
+
 T = int(input())
 
 for i in range(1,T+1):
@@ -8,15 +23,12 @@ for i in range(1,T+1):
     num_list = list(map(int,input().split()))
     
     num_list = sorted(num_list)
-    num_list_reverse = sorted(num_list, reverse = True)
+ 
     new_list = []
     for j in range(5):
-        new_list.append(num_list_reverse[j])
+        new_list.append(num_list[-(j+1)])
         new_list.append(num_list[j])
     
     print(f'#{i} {" ".join(str(x) for x in new_list)}')
-
-
-    
 
     
