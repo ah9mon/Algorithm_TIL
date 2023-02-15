ce432a0beba8c576940905fa8f7897dbcc189739
ah9mon <eomgyeol97@gmail.com>
2023-02-16 01:01:54 +0900

    2023-02-16 Dawn

--- SWEA/D2/1859.py
+++ SWEA/D2/1859.py
@@ -5,24 +5,44 @@
 # 먼저 매매가의 최댓값 찾고 그 최댓값의 인덱스 x 찾기 
 # 1 ~ x 번째 날까지 모으고 있다가 x번째날 한번에 팔기 
 #  바텀업  
+import sys
+sys.stdin = open('input_for_1859.txt')
 
-def dp(N,price):
-    d = [0]*(N+1)
-    container = 0
-    for i in range(1,N+1):
-        
-        if price[i] == max(price[i:]):
-            d[i] = d[i-1] + price[i]
-        else: 
-            d[i] = 0
-            container += 1
+# dp로 풀었는데 왜 ,, 답은 똑같은데 런타임에러나오는지 ,,,
+# def dp(N,price):
+#     d = [0]*(N+1)
+#     # max함수랑 인덱스 찾는 함수는
+#     # 데이터가 커질수록 시간 오래걸리고 + 메모리 많이씀
+#     # 따라서 반복문에 최소한으로 넣어줘야함 
+#     max_price = max(price)
+  
+#     for i in range(1,N+1):
 
-    return d[N]
+#         if price[i] != max_price: # i가 최대 가격날이 아니면 
+#             d[i] = d[i-1] + max_price - price[i] # 그날의 예상 최대 이익 = 전날 이익 + 최대가격 - 현재가격
+
+#         elif price[i] == max_price: # i가 최고가 날이면
+#             d[i] = d[i-1] # 다음날부터 가격 떨어지니까 안사는게 이득
+#             max_price = max(price[i+1:])
+
+#     return d[N]
 
 T = int(input())
 
 for i in range(1,T+1):
     N = int(input())
-    price = [0] + list(map(int,input().split()))
-    print()
+    price = list(map(int,input().split())) 
+    # print(f'#{i} {dp(N,price)}')
+    max_price = -1
+    rlt = 0
+    for j in range(N-1,-1,-1):
+        if price[j] > max_price:
+            max_price = price[j]
+        else:
+            rlt += max_price - price[j]
+    
+    print(f'#{i} {rlt}')
+            
+            
+        
     
